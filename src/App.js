74f7deeab07beba7ad09602d4356a2c086f07540
harini <harinisiva1997@gmail.com>
2021-05-23 23:53:16 +0530

    call Back dependencies, closer look at state amd comp,useMemo

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
    const [showpara,setShowPara] = useState(false)
+   const [allowToggle,setAllowToggle] = useState(false)
 
    const toggleparahandler = useCallback(()  => {
+     if(allowToggle){
      setShowPara(showpara=>!showpara)
-   },[]);
+     }
+   },[allowToggle]);
+
    console.log("app.js running")
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}></DemoOutput>
+      <DemoOutput show={showpara}></DemoOutput>
+      <Button onClick={()=>{setAllowToggle(true)}}>allow toggle</Button>
       <Button onClick={toggleparahandler}>toggle para</Button>
     </div>
   );
