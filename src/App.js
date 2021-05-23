3286fc0bf39270f71a7cac543c1f3434864f0ddd
harini <harinisiva1997@gmail.com>
2021-05-23 12:03:21 +0530

    react memo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import Button from './components/UI/Button/Button'
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+   const [showpara,setShowPara] = useState(false)
+
+   const toggleparahandler = props => {
+     setShowPara(showpara=>!showpara)
+   }
+   console.log("app.js running")
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={false}></DemoOutput>
+      <Button onClick={toggleparahandler}>toggle para</Button>
     </div>
   );
 }
 
 export default App;
+
